7ded33dfada9a6843c0576c6e78dd55cbc631a2e
rickycanonigo <rickycanonigo58@gmail.com>
2023-05-30 15:12:53 +0800

    Update DispensedMedicines.php

--- app/Models/DispensedMedicines.php
+++ app/Models/DispensedMedicines.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Database\Factories\DipensedMedicinesFactory;
 
 class DispensedMedicines extends Model
 {
     use HasFactory;
+
     protected $table = 'dispensed_medicines';
-    protected $fillable = ['medicine_id','patient_form_id','patient_name','quantity','remarks','created_at','updated_at'];
+    protected $fillable = ['medicine_id', 'patient_form_id', 'patient_name', 'quantity', 'remarks', 'created_at', 'updated_at'];
+
+    /** @return DipensedMedicinesFactory */
+    protected static function newFactory()
+    {
+        return DipensedMedicinesFactory::new();
+    }
 }
